7389fb9ab0b319ef4f8e7c0cb1baf94090ec793b
adarsh112221 <adarsh.19b401007@abes.ac.in>
2021-06-03 12:24:40 +0530

    added increase and decrease functionality

--- src/CartItem.js
+++ src/CartItem.js
@@ -12,13 +12,21 @@ class CartItem extends React.Component {
     }
     increasecounter=()=>
     {
-        console.log(this.state)
+    // 1st way->this.setState({Qty:this.state.Qty+1})
+    this.setState((prevState)=>{
+        return {Qty:prevState.Qty+1 }
+    })
+    }
+    decreasecounter=()=>
+    {
+        this.setState((prevState)=>{
+            return {Qty:prevState.Qty-1}
+        })
     }
     render() {
         const { price, title, Qty } = this.state;
         return (
             <div className="cart-item" >
-             
                 <div className="left-block">
                     <img style={style.images} />
                 </div>
@@ -28,7 +36,7 @@ class CartItem extends React.Component {
                     <div style={{ color: '#ccc', fontSize: 25 }}>Qty: {Qty}</div>
                     <div className="cart-item-action" >
                         <img className="action-icons" src="https://image.flaticon.com/icons/png/512/3303/3303893.png" alt="increase" onClick={this.increasecounter}/>
-                        <img className="action-icons" src="https://image.flaticon.com/icons/png/512/2740/2740679.png" alt="decrease" />
+                        <img className="action-icons" src="https://image.flaticon.com/icons/png/512/2740/2740679.png" alt="decrease" onClick={this.decreasecounter}/>
                         <img className="action-icons" src="https://image.flaticon.com/icons/png/512/3161/3161358.png" alt="delete" />
                     </div>
                 </div>
