dc0c468d989ce92122c3902374a4a76020f7c717
adarsh112221 <adarsh.19b401007@abes.ac.in>
2021-06-05 18:58:06 +0530

    added props

--- src/Cart.js
+++ src/Cart.js
@@ -1,13 +1,50 @@
 import React from 'react'
 import CartItem from './CartItem'
 class Cart extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            products: [{
+            price: 999,
+            title: 'watch',
+            Qty: 1,
+            img:'',
+            id:1
+        },
+        {
+             price: 999,
+             title: 'TV',
+             Qty: 1,
+             img:'',
+             id:2
+        },
+        {
+            price: 9999,
+            title: 'phone',
+            Qty: 1,
+            img:'',
+            id:3
+        }]
+        }
+    }
 
-    render() { 
+        
+       
+    
+
+    render() {
+       
+        const {products}=this.state
+      
         return (
+             
             <div className="Cart">
-            <CartItem/>
-            <CartItem/>
-            <CartItem/>
+                {
+                products.map((products)=>
+                {
+                    return <CartItem product={products} key={products.id} jsx={<h1>test</h1>}/>
+                })
+                }
             </div>
         );
     }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,15 +1,5 @@
 import React from 'react'
 class CartItem extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            price: 999,
-            title: 'phone',
-            Qty: 1
-
-        }
-        // this.increasecounter=this.increasecounter.bind(this)
-    }
     increasecounter=()=>
     {
     this.setState( (prev)=>{
@@ -42,16 +32,17 @@ class CartItem extends React.Component {
 
     // }
     
-    render() { console.log("render")
-        const { price, title, Qty } = this.state;
+    render() { console.log("props",this.props)
+        const { price, title, Qty } = this.props.product;
         return (
              
             <div className="cart-item" >
+          
                 <div className="left-block">
                     <img style={style.images} />
                 </div>
                 <div className="right-block">
-                    <div style={{ color: '#ccc', fontSize: 25 }}>Mobile {title}</div>
+                    <div style={{ color: '#ccc', fontSize: 25 }}> {title}</div>
                     <div style={{ color: '#ccc', fontSize: 25 }}>Rs {price}</div>
                     <div style={{ color: '#ccc', fontSize: 25 }}>Qty: {Qty}</div>
                     <div className="cart-item-action" >
